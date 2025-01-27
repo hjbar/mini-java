fb014c00e13a6fe42843efa50fa7d48db6fe8abf
hjbar <hjbar@protonmail.com>
2025-01-27 16:30:13 +0100

    Update setup

--- src/typing.ml
+++ src/typing.ml
@@ -1,20 +1,17 @@
-
 open Ast
 
 let debug = ref false
 
-let dummy_loc = Lexing.dummy_pos, Lexing.dummy_pos
+let dummy_loc = (Lexing.dummy_pos, Lexing.dummy_pos)
 
 exception Error of Ast.location * string
 
 (* use the following function to signal typing errors, e.g.
       error ~loc "unbound variable %s" id
 *)
-let error ?(loc=dummy_loc) f =
+let error ?(loc = dummy_loc) f =
   Format.kasprintf (fun s -> raise (Error (loc, s))) ("@[" ^^ f ^^ "@]")
 
-
-let file ?debug:(b=false) (p: Ast.pfile) : Ast.tfile =
+let file ?debug:(b = false) (p : Ast.pfile) : Ast.tfile =
   debug := b;
   failwith "TODO"
-
